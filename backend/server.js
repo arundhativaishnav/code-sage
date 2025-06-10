814a5232ce51cdcdf9b509a3f4fd73427d8df1c3
arundhativaishnav <arundhatianil17@gmail.com>
2025-06-11 01:23:01 +0530

    url changed in frontend

--- backend/server.js
+++
@@ -1,7 +0,0 @@
-const app = require("./src/App")
-require('dotenv').config()
-
-
-app.listen(3000 , () => {
-    console.log("Server is running on http://localhost:3000")
-})
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -18,7 +18,7 @@ function App() {
   }, []);
 
   async function reviewCode() {
-    const response = await axios.post('http://localhost:3000/ai/get-review', { code });
+    const response = await axios.post('https://codesage-backend-o6g2.onrender.com/ai/get-review', { code });
     setReview(response.data);
   }
 
