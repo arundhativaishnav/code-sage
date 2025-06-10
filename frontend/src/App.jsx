11325e9a1995abaa29ac811e167b666476754e23
arundhativaishnav <arundhatianil17@gmail.com>
2025-06-11 00:13:41 +0530

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,59 @@
+import { useState, useEffect } from 'react';
+import "prismjs/themes/prism-tomorrow.css";
+import Editor from "react-simple-code-editor";
+import prism from 'prismjs';
+import "prismjs/components/prism-jsx";
+import rehypeHighlight from "rehype-highlight";
+import "highlight.js/styles/github-dark.css";
+import axios from 'axios';
+import './App.css';
+import Markdown from 'react-markdown';
+
+function App() {
+  const [code, setCode] = useState([]);
+  const [review, setReview] = useState("");
+
+  useEffect(() => {
+    prism.highlightAll();
+  }, []);
+
+  async function reviewCode() {
+    const response = await axios.post('http://localhost:3000/ai/get-review', { code });
+    setReview(response.data);
+  }
+
+  return (
+    <>
+      <header className="app-header">
+        <h1>🧙‍♂️ Code Sage</h1>
+        <p>Your AI-Powered Code Reviewer</p>
+      </header>
+      <main>
+        <div className="left">
+          <div className="code-editor-wrapper">
+            <Editor
+              value={code}
+              onValueChange={code => setCode(code)}
+              highlight={code => prism.highlight(code, prism.languages.javascript, "javascript")}
+              padding={12}
+              style={{
+                fontFamily: '"Fira Code", monospace',
+                fontSize: 16,
+                minHeight: "100%",
+                outline: "none"
+              }}
+            />
+          </div>
+          <button onClick={reviewCode} className="review-btn">💡 Review Code</button>
+        </div>
+        <div className="right">
+          <Markdown rehypePlugins={[rehypeHighlight]}>
+            {review}
+          </Markdown>
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default App;
